ab8e2b92cafcc9b42912df044af43204b0c9af0f
zvizvi <5633910@gmail.com>
2025-09-11 19:30:05 +0300

    Add disable sidebar option to CommentsEntry

--- resources/views/filament/infolists/components/comments-entry.blade.php
+++ resources/views/filament/infolists/components/comments-entry.blade.php
@@ -7,5 +7,6 @@
         :per-page="$getPerPage()"
         :load-more-label="$getLoadMoreLabel()"
         :per-page-increment="$getPerPageIncrement()"
+        :sidebar-enabled="$isSidebarEnabled() ?? false"
     />
 </x-dynamic-component>
